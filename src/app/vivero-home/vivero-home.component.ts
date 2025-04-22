3802aced3d9028b44328b8feacbeb5de62feeda8
fiorella <difiore8fiorella@gmail.com>
2025-04-22 18:26:07 -0300

    new changes

--- src/app/vivero-home/vivero-home.component.ts
+++ src/app/vivero-home/vivero-home.component.ts
@@ -12,11 +12,11 @@ import { Component } from '@angular/core';
 export class ViveroHomeComponent {
   imagenes=
     {
-      image1: "./img/1carousel.png",
-      image2: "./img/2carousel.png",
-      image3: "./img/3carousel.png",
-      image4: "./img/mediodepago.png",
-      image5: "./img/home2.png",
-      image6: "./img/home3.png",
+      image1: "/img/1carousel.png",
+      image2: "/img/2carousel.png",
+      image3: "/img/3carousel.png",
+      image4: "/img/mediodepago.png",
+      image5: "/img/home2.png",
+      image6: "/img/home3.png",
     }
 }
