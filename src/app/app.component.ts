75570d4e1f2d7f15bc01713ab4de69eae1b6db20
fiorella <difiore8fiorella@gmail.com>
2025-04-27 19:09:55 -0300

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,32 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  showCartButton: boolean = false;
   title = 'VIVERO';
   email= 'vivero@gmail.com';
   tel= '+54 1156256987';
+
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Detecta la ruta actual cuando se inicializa el componente
+    this.checkRoute();
+  }
+
+  ngAfterViewChecked() {
+    // Revisa la ruta actual cuando la vista se actualiza
+    this.checkRoute();
+  }
+
+  // Método para verificar la ruta y mostrar el botón del carrito
+  checkRoute() {
+    // Obtiene la ruta actual y muestra el botón solo si es "catalogo"
+    const currentRoute = this.router.url;
+    this.showCartButton = currentRoute === '/plants';
+  }
 }
+
 
 
