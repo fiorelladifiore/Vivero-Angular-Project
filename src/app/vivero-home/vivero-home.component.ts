85fc29b6209b107826d38cee3f2fa90fd53b5c0c
fiorella <difiore8fiorella@gmail.com>
2025-04-27 03:42:14 -0300

    fixes

--- src/app/vivero-home/vivero-home.component.ts
+++ src/app/vivero-home/vivero-home.component.ts
@@ -15,7 +15,7 @@ export class ViveroHomeComponent {
       image1: "https://i.ibb.co/8nrXXZ1v/carousel1.jpg",
       image2: "https://i.ibb.co/20xdVghP/carousel2.jpg",
       image3: "https://i.ibb.co/cc6LS8Jj/carousel3.jpg",
-      image4: "/img/mediodepago.png",
+      image4: "https://i.ibb.co/RkkpyY1B/mediodepago.png",
       image5: "https://i.ibb.co/XB0LzqJ/image5.jpg",
       image6: "https://i.ibb.co/F43cGHDY/image6.jpg",
       image7: "https://i.ibb.co/whTnHwHT/image7.jpg",
